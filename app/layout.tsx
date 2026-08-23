8c7273fc98ca4dc2bcdca70822da2c48a7c72a7e
biloa2005 <biloaphilemon@gmail.com>
2026-08-23 20:31:42 +0200

    bouton pour contacter whatsapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { Analytics } from '@vercel/analytics/react';
+import WhatsAppButton from "@/components/WhatsAppButton";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -63,7 +64,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}
-     
+     <WhatsAppButton/>
         <Analytics /> 
       </body>
     </html>
---
+++ components/WhatsAppButton.tsx
@@ -0,0 +1,21 @@
+import { MessageCircle } from 'lucide-react';
+import React from 'react'
+
+export default function WhatsAppButton() {
+  const phoneNumber="237696127469"
+  const message="Bonjour je souhaite avoir plus d informations.";
+  const whatsappurl=`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    return (
+    <a
+    href={whatsappurl}
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label="Nous contacter par whatsapp"
+    className="fixed bottom-30 right-6 z-50 flex h-14 w-14 items-center justify-center 
+    rounded-full bg-green-500 text-white shadow-lg transition-all duracion-300
+    hover:scale-110 hover:bg-green-600"
+    >
+<MessageCircle/>
+    </a>
+  )
+}

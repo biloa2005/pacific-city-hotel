23338f37c96b719878dec0a482718b1cba2be096
biloa2005 <biloaphilemon@gmail.com>
2026-09-07 23:14:47 +0200

    grille repas

--- components/Restautant/RestaurantGrid.tsx
+++ components/Restautant/RestaurantGrid.tsx
@@ -31,9 +31,9 @@ export default function RestaurantGrid() {
   }, []);
 
   return (
-    <section className="max-w-4xl mx-auto p-2 sm:p-4">
+    <section className="max-w-3xl mx-auto p-2 sm:p-4">
       {/* Grille responsive */}
-      <div className="grid grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-1.5 sm:gap-2 lg:gap-3 max-w-4xl mx-auto">
+      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-1.5 sm:gap-1 lg:gap-2 max-w-4xl mx-auto">
         {items.map((item) => (
           <button
             key={item.id}

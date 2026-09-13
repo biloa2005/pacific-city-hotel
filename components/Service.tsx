021c4b13bb3ca18feecef2e4997cc04da0de9038
biloa2005 <biloaphilemon@gmail.com>
2026-09-13 14:10:06 +0200

    couleur dorer pour les services de l'hotel

--- components/Service.tsx
+++ components/Service.tsx
@@ -170,9 +170,11 @@ export default function ServicesSection() {
                 }`}
               >
                 {/* Icône */}
-                <div className="flex h-12 w-12 flex-shrink-0 items-center justify-center bg-[#D4AF37]/10  transition-all duration-300 group-hover:bg-[#D4AF37] group-hover:text-white">
-                  {service.icon}
-                </div>
+               {/* Icône avec couleur dorée par défaut */}
+<div className="flex h-12 w-12 flex-shrink-0 items-center justify-center bg-[#D4AF37]/10 text-[#D4AF37] transition-all duration-300 group-hover:bg-[#D4AF37] group-hover:text-white">
+  {service.icon}
+</div>
+
 
                 {/* Titre */}
                 <h3 className="text-sm font-semibold text-gray-900 transition-colors duration-300 group-hover:text-[#D4AF37] md:text-base">
